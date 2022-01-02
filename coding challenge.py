c52f49f2b45ca0b8dffb8c4f52e8ef77fd46f46e
Christina Li <cli240@student.monash.edu>
2022-01-02 21:42:53 +1100

    first iteration different

--- coding challenge.py
+++ coding challenge.py
@@ -1,27 +1,39 @@
 import re
 x:int
 y:int
-f = "NORTH"
+f = "first"
 
 while True:
-    # if x and y are not set, continue to next iteration of loop
     val = input("Please give command (one of PLACE, MOVE, LEFT, RIGHT, REPORT)\n").upper()
     val = re.split(' |,| ', val)
-    filteredVal = list(filter(None, val))
-
-    print("input", filteredVal)
-    
+    filteredVal = list(filter(None, val))    
     command = filteredVal[0]
 
     match command:
         case "PLACE":
-            x = int(filteredVal[1])
-            y = int(filteredVal[2])
+            if len(filteredVal) != 4:
+                print("Invalid number of input")
+                continue
+            try:
+                x = int(filteredVal[1])
+            except ValueError:
+                print("x: Please enter an integer between 0 and 5")
+                continue
+
+            try:
+                y = int(filteredVal[2])
+            except ValueError:
+                print("y: Please enter an integer between 0 and 5")
+                continue           
+            
             f = filteredVal[3]
-            print(filteredVal)
-            print(x, y)
+            if f not in ["NORTH", "EAST", "SOUTH", "WEST"]:
+                print("Invalid direction, please enter one of North, South, East, or West")
+                continue
+
         case "MOVE":
-             # print("before move", x, y, f )
+            if f == "first":
+                continue
             match f: 
                 case "NORTH": 
                     if y + 1 > 5: 
@@ -43,9 +55,10 @@ while True:
                         print("Robot doesn't want to fall off the table, please give another command")
                         continue
                     x = x - 1
-            print("after move", x, y, f)
+
         case "LEFT":
-            print("before turn", x, y, f)
+            if f == "first":
+                continue
             match f: 
                 case "NORTH": 
                     f = "WEST"
@@ -55,9 +68,10 @@ while True:
                     f = "EAST"
                 case "WEST":
                     f = "SOUTH"
-            print("after turn", f)
+
         case "RIGHT":
-            print("before turn", f)
+            if f == "first":
+                continue
             match f: 
                 case "NORTH": 
                     f = "EAST"
@@ -67,6 +81,11 @@ while True:
                     f = "WEST"
                 case "WEST":
                     f = "NORTH"
-            print("after turn", x, y, f)
+
         case "REPORT":
+            if f == "first":
+                continue
             print(x, ",", y, f)
+            
+        case _:
+            print("Not a valid command")
