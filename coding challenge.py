0af0d9f6a6c993922d0f77c7dde131f83a43c40b
Christina Li <cli240@student.monash.edu>
2022-01-02 20:51:33 +1100

    if else -> match case

--- coding challenge.py
+++ coding challenge.py
@@ -13,61 +13,60 @@ while True:
     
     command = filteredVal[0]
 
-    if command == "PLACE":
-        x = int(filteredVal[1])
-        y = int(filteredVal[2])
-        f = filteredVal[3]
-        print(filteredVal)
-        print(x, y)
-    if command == "MOVE":
-        # print("before move", x, y, f )
-        match f: 
-            case "NORTH": 
-                if y + 1 > 5: 
-                    print("Robot doesn't want to fall off the table, please give another command")
-                    continue
-                y = y + 1
-            case "EAST": 
-                if x + 1 > 5: 
-                    print("Robot doesn't want to fall off the table, please give another command")
-                    continue
-                x = x + 1
-            case "SOUTH":
-                if y - 1 < 0: 
-                    print("Robot doesn't want to fall off the table, please give another command")
-                    continue
-                y = y - 1
-            case "WEST":
-                if x - 1 < 0: 
-                    print("Robot doesn't want to fall off the table, please give another command")
-                    continue
-                x = x - 1
-        print("after move", x, y, f)
-
-    if command == "LEFT": 
-        print("before turn", x, y, f)
-        match f: 
-            case "NORTH": 
-                f = "WEST"
-            case "EAST": 
-                f = "NORTH"
-            case "SOUTH":
-                f = "EAST"
-            case "WEST":
-                f = "SOUTH"
-        print("after turn", f)
-    
-    if command == "RIGHT": 
-        print("before turn", f)
-        match f: 
-            case "NORTH": 
-                f = "EAST"
-            case "EAST": 
-                f = "SOUTH"
-            case "SOUTH":
-                f = "WEST"
-            case "WEST":
-                f = "NORTH"
-        print("after turn", x, y, f)
-    if command == "REPORT": 
-        print(x, ",", y, f)
+    match command:
+        case "PLACE":
+            x = int(filteredVal[1])
+            y = int(filteredVal[2])
+            f = filteredVal[3]
+            print(filteredVal)
+            print(x, y)
+        case "MOVE":
+             # print("before move", x, y, f )
+            match f: 
+                case "NORTH": 
+                    if y + 1 > 5: 
+                        print("Robot doesn't want to fall off the table, please give another command")
+                        continue
+                    y = y + 1
+                case "EAST": 
+                    if x + 1 > 5: 
+                        print("Robot doesn't want to fall off the table, please give another command")
+                        continue
+                    x = x + 1
+                case "SOUTH":
+                    if y - 1 < 0: 
+                        print("Robot doesn't want to fall off the table, please give another command")
+                        continue
+                    y = y - 1
+                case "WEST":
+                    if x - 1 < 0: 
+                        print("Robot doesn't want to fall off the table, please give another command")
+                        continue
+                    x = x - 1
+            print("after move", x, y, f)
+        case "LEFT":
+            print("before turn", x, y, f)
+            match f: 
+                case "NORTH": 
+                    f = "WEST"
+                case "EAST": 
+                    f = "NORTH"
+                case "SOUTH":
+                    f = "EAST"
+                case "WEST":
+                    f = "SOUTH"
+            print("after turn", f)
+        case "RIGHT":
+            print("before turn", f)
+            match f: 
+                case "NORTH": 
+                    f = "EAST"
+                case "EAST": 
+                    f = "SOUTH"
+                case "SOUTH":
+                    f = "WEST"
+                case "WEST":
+                    f = "NORTH"
+            print("after turn", x, y, f)
+        case "REPORT":
+            print(x, ",", y, f)
